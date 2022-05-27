abdc611603f393584a32a8c2c290fad4686cb88d
f1703 <franco170391@gmail.com>
2022-05-26 23:12:17 -0300

    ejercicios

---
+++ sueltos/ejercicio01.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# Haga un shell script que devuelva los argumentos primero y ultimo que se han pasado
+# 
+# Si se llama con: 
+# archivo.sh hola como estas
+# 
+# debe responder:
+# El primer argumento es hola
+# El ultimo argumento es estas
+#
+ 
+for last in $* ; do true
+done
+
+for arg in $* ; do
+    if [ $arg = $1 ]; then
+        echo "El primer argumento es $arg"
+    fi
+done
+
+echo  "El ultimo argumento es $last"
+ 
